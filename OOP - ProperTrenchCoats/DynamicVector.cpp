0dc3d539c24290b259260de22c24e45bf17ff724
Adrian Mihu <mihu.adrian97@gmail.com>
2017-04-25 11:56:30 +0300

    DynamicVector uses generic templates

--- OOP - ProperTrenchCoats/DynamicVector.cpp
+++ OOP - ProperTrenchCoats/DynamicVector.cpp
@@ -1,94 +1,94 @@
-#include "DynamicVector.h"
-
-DynamicVector::DynamicVector(int capacity)
-{
-	this->size = 0;
-	this->capacity = capacity;
-	this->elems = new TElement[capacity];
-}
-
-DynamicVector::DynamicVector(const DynamicVector & v)
-{
-	this->size = v.size;
-	this->capacity = v.capacity;
-	this->elems = new TElement[this->capacity];
-	for (int i = 0; i < this->size; i++)
-		this->elems[i] = v.elems[i];
-}
-
-DynamicVector::~DynamicVector()
-{
-	delete[] this->elems;
-}
-
-DynamicVector& DynamicVector::operator=(const DynamicVector& v)
-{
-	if (this == &v)
-		return *this;
-
-	this->size = v.size;
-	this->capacity = v.capacity;
-
-	delete[] this->elems;
-	this->elems = new TElement[this->capacity];
-	for (int i = 0; i < this->size; i++)
-		this->elems[i] = this->elems[i];
-
-	return *this;
-}
-
-DynamicVector & DynamicVector::operator-(const TElement & e)
-{
-	bool found = false;
-	int i = 0;
-	while (found == false && i<size) {
-		if (elems[i] == e)
-			found = true;
-		else
-			i++;
-	}
-	remove(i);
-	return *this;
-}
-
-void DynamicVector::resize(double factor)
-{
-	this->capacity *= static_cast<int>(factor);
-
-	TElement* els = new TElement[this->capacity];
-	for (int i = 0; i < this->size; i++)
-		els[i] = this->elems[i];
-
-	delete[] this->elems;
-	this->elems = els;
-}
-
-void DynamicVector::add(const TElement& e)
-{
-	if (this->size == this->capacity)
-		this->resize();
-	this->elems[this->size] = e;
-	this->size++;
-}
-
-void DynamicVector::remove(const int poz)
-{
-	for (int i = poz; i < this->size; i++)
-		this->elems[i] = this->elems[i + 1];
-	this->size--;
-}
-
-void DynamicVector::update(const int poz, const TElement& e)
-{
-	this->elems[poz] = e;
-}
-
-TElement* DynamicVector::getAllElems() const
-{
-	return this->elems;
-}
-
-int DynamicVector::getSize() const
-{
-	return this->size;
-}
+//#include "DynamicVector.h"
+//
+//DynamicVector::DynamicVector(int capacity)
+//{
+//	this->size = 0;
+//	this->capacity = capacity;
+//	this->elems = new TElement[capacity];
+//}
+//
+//DynamicVector::DynamicVector(const DynamicVector & v)
+//{
+//	this->size = v.size;
+//	this->capacity = v.capacity;
+//	this->elems = new TElement[this->capacity];
+//	for (int i = 0; i < this->size; i++)
+//		this->elems[i] = v.elems[i];
+//}
+//
+//DynamicVector::~DynamicVector()
+//{
+//	delete[] this->elems;
+//}
+//
+//DynamicVector& DynamicVector::operator=(const DynamicVector& v)
+//{
+//	if (this == &v)
+//		return *this;
+//
+//	this->size = v.size;
+//	this->capacity = v.capacity;
+//
+//	delete[] this->elems;
+//	this->elems = new TElement[this->capacity];
+//	for (int i = 0; i < this->size; i++)
+//		this->elems[i] = this->elems[i];
+//
+//	return *this;
+//}
+//
+//DynamicVector & DynamicVector::operator-(const TElement & e)
+//{
+//	bool found = false;
+//	int i = 0;
+//	while (found == false && i<size) {
+//		if (elems[i] == e)
+//			found = true;
+//		else
+//			i++;
+//	}
+//	remove(i);
+//	return *this;
+//}
+//
+//void DynamicVector::resize(double factor)
+//{
+//	this->capacity *= static_cast<int>(factor);
+//
+//	TElement* els = new TElement[this->capacity];
+//	for (int i = 0; i < this->size; i++)
+//		els[i] = this->elems[i];
+//
+//	delete[] this->elems;
+//	this->elems = els;
+//}
+//
+//void DynamicVector::add(const TElement& e)
+//{
+//	if (this->size == this->capacity)
+//		this->resize();
+//	this->elems[this->size] = e;
+//	this->size++;
+//}
+//
+//void DynamicVector::remove(const int poz)
+//{
+//	for (int i = poz; i < this->size; i++)
+//		this->elems[i] = this->elems[i + 1];
+//	this->size--;
+//}
+//
+//void DynamicVector::update(const int poz, const TElement& e)
+//{
+//	this->elems[poz] = e;
+//}
+//
+//TElement* DynamicVector::getAllElems() const
+//{
+//	return this->elems;
+//}
+//
+//int DynamicVector::getSize() const
+//{
+//	return this->size;
+//}
